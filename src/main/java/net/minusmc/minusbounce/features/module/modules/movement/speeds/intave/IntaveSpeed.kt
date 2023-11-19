9c4b0aa51362857c836a347c38d179c92ac58d5a
TuanSoCuLa <tokenspam9@gmail.com>
2023-11-19 14:43:32 +0700

    Fix

--- src/main/java/net/minusmc/minusbounce/features/module/modules/movement/speeds/intave/IntaveSpeed.kt
+++ src/main/java/net/minusmc/minusbounce/features/module/modules/movement/speeds/intave/IntaveSpeed.kt
@@ -1,32 +1,25 @@
 package net.minusmc.minusbounce.features.module.modules.movement.speeds.intave
 
-import net.minusmc.minusbounce.MinusBounce
-import net.minusmc.minusbounce.event.MotionEvent
 import net.minusmc.minusbounce.features.module.modules.movement.speeds.SpeedType
 import net.minusmc.minusbounce.features.module.modules.movement.speeds.SpeedMode
 import net.minusmc.minusbounce.utils.MovementUtils
 import net.minecraft.client.settings.GameSettings
 
 class IntaveSpeed: SpeedMode("Intave", SpeedType.INTAVE) {
-    var offGroundTicks = 0
-
-    override fun onMotion(event: MotionEvent) {
-        if(!mc.thePlayer.onGround)
-            offGroundTicks++
-        else offGroundTicks = 0
-
-        if(mc.thePlayer.onGround) {
-            mc.thePlayer.jump()
-        }
-
-        if (offGroundTicks >= 10) {
-            MovementUtils.setMoveSpeed(MovementUtils.getSpeed().toDouble())
-        }
-
-        if (mc.thePlayer.motionY > 0.003) {
-            mc.thePlayer.motionX *= 1.0015
-            mc.thePlayer.motionZ *= 1.0015
-            mc.timer.timerSpeed = 1.1f
+    override fun onUpdate() {
+        mc.gameSettings.keyBindJump.pressed = GameSettings.isKeyDown(mc.gameSettings.keyBindJump)
+        if (MovementUtils.isMoving) {
+            if (mc.thePlayer.onGround) {
+                mc.gameSettings.keyBindJump.pressed = false
+                mc.timer.timerSpeed = 1f
+                mc.thePlayer.jump()
+            }
+            if (mc.thePlayer.motionY > 0.003) {
+                mc.thePlayer.motionX *= 1.0015
+                mc.thePlayer.motionZ *= 1.0015
+                mc.timer.timerSpeed = 1.06f
+            }
         }
     }
+
 }
