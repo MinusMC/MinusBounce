5812cb33ff837fc934061ae2c75ebada96d225d3
TuanSoCuLa <tokenspam9@gmail.com>
2023-11-19 14:55:32 +0700

    Fix

--- src/main/java/net/minusmc/minusbounce/features/module/modules/combat/velocitys/grim/GrimC07Velocity.kt
+++ src/main/java/net/minusmc/minusbounce/features/module/modules/combat/velocitys/grim/GrimC07Velocity.kt
@@ -1,5 +1,10 @@
 package net.minusmc.minusbounce.features.module.modules.combat.velocitys.grim
 
+import net.minusmc.minusbounce.event.*
+import net.minusmc.minusbounce.features.module.modules.combat.velocitys.VelocityMode
+import net.minusmc.minusbounce.features.value.BoolValue
+import net.minusmc.minusbounce.features.value.IntegerValue
+import net.minusmc.minusbounce.utils.timer.MSTimer
 import net.minecraft.network.play.client.C03PacketPlayer
 import net.minecraft.network.play.client.C07PacketPlayerDigging
 import net.minecraft.network.play.server.S08PacketPlayerPosLook
@@ -8,23 +13,22 @@ import net.minecraft.network.play.server.S27PacketExplosion
 import net.minecraft.util.BlockPos
 import net.minecraft.util.EnumFacing
 import net.minecraft.world.World
-import net.minusmc.minusbounce.event.PacketEvent
-import net.minusmc.minusbounce.event.TickEvent
-import net.minusmc.minusbounce.features.module.modules.combat.velocitys.VelocityMode
-import net.minusmc.minusbounce.utils.timer.MSTimer
-import net.minusmc.minusbounce.value.BoolValue
-import net.minusmc.minusbounce.value.IntegerValue
 
 class GrimC07Velocity : VelocityMode("GrimC07") {
-    private val alwaysValue = BoolValue("Always", true)
-    private val onlyAirValue = BoolValue("OnlyBreakAir", true)
-    private val worldValue = BoolValue("BreakOnWorld", false)
-    private val sendC03Value = BoolValue("SendC03", false)
-    private val c06Value = BoolValue("Send1.17C06", false) { sendC03Value.get() }
+    private val alwaysValue = BoolValue("${valuePrefix}Always", true)
+
+    private val onlyAirValue = BoolValue("${valuePrefix}OnlyBreakAir", true)
+
+    private val worldValue = BoolValue("${valuePrefix}BreakOnWorld", false)
+
+    private val sendC03Value = BoolValue("${valuePrefix}SendC03", false).displayable { false } // bypass latest but flag timer
+
+    private val C06Value = BoolValue("${valuePrefix}Send1.17C06", false).displayable { sendC03Value.get() } // need via to 1.17+
+
     private val flagPauseValue = IntegerValue("FlagPause-Time", 50, 0, 5000)
 
-    private var gotVelo = false
-    private var flagTimer = MSTimer()
+    var gotVelo = false
+    var flagTimer = MSTimer()
 
     override fun onEnable() {
         gotVelo = false
@@ -60,23 +64,23 @@ class GrimC07Velocity : VelocityMode("GrimC07") {
         val theWorld = mc.theWorld ?: return
         if (gotVelo || alwaysValue.get()) { // packet processed event pls
             val pos = BlockPos(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ)
-            if (checkBlock(pos, theWorld) || checkBlock(pos.up(), theWorld)) {
-                if (sendC03Value.get()) {
-                    if (c06Value.get())
-                        mc.netHandler.addToSendQueue(C03PacketPlayer.C06PacketPlayerPosLook(thePlayer.posX, thePlayer.posY, thePlayer.posZ, thePlayer.rotationYaw, thePlayer.rotationPitch, thePlayer.onGround))
-                    else
-                        mc.netHandler.addToSendQueue(C03PacketPlayer(thePlayer.onGround))
-                }
+            if (checkBlock(pos) || checkBlock(pos.up())) {
+                gotVelo = false
             }
-            gotVelo = false
         }
     }
 
-    private fun checkBlock(pos: BlockPos, theWorld: World): Boolean {
-        if (!onlyAirValue.get() || theWorld.isAirBlock(pos)) {
+    fun checkBlock(pos: BlockPos): Boolean {
+        if (!onlyAirValue.get() || mc.theWorld.isAirBlock(pos)) {
+            if (sendC03Value.get()) {
+                if (C06Value.get())
+                    mc.netHandler.addToSendQueue(C03PacketPlayer.C06PacketPlayerPosLook(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ, mc.thePlayer.rotationYaw, mc.thePlayer.rotationPitch, mc.thePlayer.onGround))
+                else
+                    mc.netHandler.addToSendQueue(C03PacketPlayer(mc.thePlayer.onGround))
+            }
             mc.netHandler.addToSendQueue(C07PacketPlayerDigging(C07PacketPlayerDigging.Action.STOP_DESTROY_BLOCK, pos, EnumFacing.DOWN))
             if (worldValue.get())
-                theWorld.setBlockToAir(pos)
+                mc.theWorld.setBlockToAir(pos)
             return true
         }
         return false
