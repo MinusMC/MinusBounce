9cbe218008cefae9f13fe18307c585392cffeea6
toidicakhia <toidicakhia1234@gmail.com>
2023-11-20 10:59:00 +0700

    fix

--- src/main/java/net/minusmc/minusbounce/features/module/modules/movement/speeds/verus/VerusYPort3Speed.kt
+++ src/main/java/net/minusmc/minusbounce/features/module/modules/movement/speeds/verus/VerusYPort3Speed.kt
@@ -4,20 +4,25 @@ import net.minusmc.minusbounce.features.module.modules.movement.speeds.SpeedMode
 import net.minusmc.minusbounce.features.module.modules.movement.speeds.SpeedType
 import net.minusmc.minusbounce.utils.MovementUtils
 
-class VerusYPort3Speed: SpeedMode("Verus YPort 3", SpeedType.VERUS) {
+class VerusYPort3Speed: SpeedMode("VerusYPort3", SpeedType.VERUS) {
     private var verusTick = 0
+
+    override fun onDisable() {
+        verusTick = 0
+    }
 
     override fun onUpdate() {
         if (MovementUtils.isMoving && mc.thePlayer.onGround && verusTick == 0) {
-            mc.thePlayer.motionY += 0.42
+            mc.thePlayer.motionY = 0.42
             verusTick = 1
         } else if (verusTick == 1) {
-            MovementUtils.strafe(0.3f)
+            verusTick = 2
+            MovementUtils.strafe(0.2f)
         } else if (verusTick == 5) {
             verusTick = 0
         } else if (verusTick < 5) {
             verusTick++
-            mc.thePlayer.motionY -= 0.21291
+            mc.thePlayer.motionY -= 0.12
         }
     }
 }
