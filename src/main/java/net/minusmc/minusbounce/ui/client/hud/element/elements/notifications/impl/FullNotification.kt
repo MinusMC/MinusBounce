57575d28da7788f9d3000c6c5960ac2c6e649722
toidicakhia <toidicakhia1234@gmail.com>
2023-12-11 18:59:54 +0700

    fix tap 4

--- src/main/java/net/minusmc/minusbounce/ui/client/hud/element/elements/notifications/impl/FullNotification.kt
+++ src/main/java/net/minusmc/minusbounce/ui/client/hud/element/elements/notifications/impl/FullNotification.kt
@@ -1,11 +1,16 @@
 package net.minusmc.minusbounce.ui.client.hud.element.elements.notifications
 
+import net.minusmc.minusbounce.ui.client.hud.element.elements.Notifications
+import net.minusmc.minusbounce.ui.client.hud.element.elements.Notification
 import net.minusmc.minusbounce.ui.client.hud.element.elements.Notification.Type
+import net.minusmc.minusbounce.ui.client.hud.element.elements.Notification.FadeState
+import net.minusmc.minusbounce.ui.client.hud.element.Border
 import net.minusmc.minusbounce.ui.font.Fonts
 import net.minusmc.minusbounce.utils.render.Stencil
 import net.minusmc.minusbounce.utils.render.RenderUtils
 import net.minusmc.minusbounce.utils.render.BlurUtils
 import net.minecraft.client.renderer.GlStateManager
+import net.minecraft.util.ResourceLocation
 import org.lwjgl.opengl.GL11
 import java.awt.Color
 
@@ -17,7 +22,22 @@ class FullNotification: NotificationStyle("Full") {
     private val imgWarning = ResourceLocation("${notifyDir}warning.png")
     private val imgInfo = ResourceLocation("${notifyDir}info.png")
 
-	override fun drawStyle() {
+	override fun drawStyle(notification: Notification, y: Float) {
+        val x = notification.x
+        val originalX = inst.renderX.toFloat()
+        val originalY = inst.renderY.toFloat()
+        val textLength = notification.textLength
+        val blur = inst.blurValue.get()
+        val strength = inst.blurStrength.get()
+        val backgroundColor = Color(0, 0, 0, inst.bgAlphaValue.get())
+
+        val enumColor = when (notification.type) {
+            Type.SUCCESS -> Color(80, 255, 80).rgb
+            Type.ERROR -> Color(255, 80, 80).rgb
+            Type.INFO -> Color(255, 255, 255).rgb
+            Type.WARNING -> Color(255, 255, 0).rgb
+        }
+
 		val dist = (x + 1 + 26F) - (x - 8 - textLength)
         val kek = -x - 1 - 26F
 
@@ -45,12 +65,12 @@ class FullNotification: NotificationStyle("Full") {
         GL11.glPopMatrix()
 
         GlStateManager.resetColor()
-        if (fadeState == FadeState.STAY && !stayTimer.hasTimePassed(displayTime))
-            RenderUtils.drawRect(kek, -y, kek + (dist * if (stayTimer.hasTimePassed(displayTime)) 0F else ((displayTime - (System.currentTimeMillis() - stayTimer.time)).toFloat() / displayTime.toFloat())), -1F - y, enumColor)
-        else if (fadeState == FadeState.IN)
+        if (notification.fadeState == FadeState.STAY && !stayTimer.hasTimePassed(notification.displayTime))
+            RenderUtils.drawRect(kek, -y, kek + (dist * if (stayTimer.hasTimePassed(notification.displayTime)) 0F else ((notification.displayTime - (System.currentTimeMillis() - stayTimer.time)).toFloat() / notification.displayTime.toFloat())), -1F - y, enumColor)
+        else if (notification.fadeState == FadeState.IN)
             RenderUtils.drawRect(kek, -y, kek + dist, -1F - y, enumColor)
 
         GlStateManager.resetColor()
-        Fonts.font40.drawString(message, -x + 2, -18F - y, -1)
+        Fonts.font40.drawString(notification.message, -x + 2, -18F - y, -1)
 	}
 }
