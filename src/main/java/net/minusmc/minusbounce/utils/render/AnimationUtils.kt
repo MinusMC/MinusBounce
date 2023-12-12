a21f79eee5ca57d721782b3671b3112f944f13ca
toidicakhia <toidicakhia1234@gmail.com>
2023-12-12 18:46:16 +0700

    revert and add novoline clickgui

--- src/main/java/net/minusmc/minusbounce/utils/render/AnimationUtils.kt
+++ src/main/java/net/minusmc/minusbounce/utils/render/AnimationUtils.kt
@@ -87,4 +87,18 @@ object AnimationUtils {
         }
         return current
     }
+
+    fun easeInBack(x: Double): Double {
+        val c1 = 1.70158
+        val c3 = c1 + 1
+
+        return c3 * x * x * x - c1 * x * x
+    }
+
+    fun easeOutBack(x: Double): Double {
+        val c1 = 1.70158
+        val c3 = c1 + 1
+
+        return 1 + c3 * (x - 1).pow(3) + c1 * (x - 1).pow(2)
+    }
 }
