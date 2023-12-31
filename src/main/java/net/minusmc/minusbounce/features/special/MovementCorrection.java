f3c1261d5420f8d4f4213502fc7ef915bec78cfc
fmcpe <minhductran784@gmail.com>
2023-12-31 10:53:18 +0700

    Not partially anymore
    I skid :(

--- src/main/java/net/minusmc/minusbounce/features/special/MovementCorrection.java
+++ src/main/java/net/minusmc/minusbounce/features/special/MovementCorrection.java
@@ -8,7 +8,7 @@ package net.minusmc.minusbounce.features.special;
 import net.minusmc.minusbounce.MinusBounce;
 import net.minusmc.minusbounce.features.module.modules.world.Scaffold;
 import net.minusmc.minusbounce.event.*;
-import net.minusmc.minusbounce.utils.MinecraftInstance;
+import net.minusmc.minusbounce.utils.*;
 import net.minecraft.util.MathHelper;
 
 import static net.minusmc.minusbounce.utils.RotationUtils.targetRotation;
@@ -20,52 +20,16 @@ public final class MovementCorrection extends MinecraftInstance implements Liste
         final Scaffold scaffold = MinusBounce.moduleManager.getModule(Scaffold.class);
 
         if (targetRotation == null) return;
+
         final float forward = event.getForward();
         final float strafe = event.getStrafe();
-        final float yaw = targetRotation.getYaw();
 
-        final double angle = MathHelper.wrapAngleTo180_double(Math.toDegrees(direction(mc.thePlayer.rotationYaw, forward, strafe)));
+        final float rotationOffset = (float) Math.toRadians(mc.thePlayer.rotationYaw - targetRotation.getYaw());
+        final float cosValue = MathHelper.cos(rotationOffset);
+        final float sinValue = MathHelper.sin(rotationOffset);
 
-        if (forward == 0 && strafe == 0) {
-            return;
-        }
-
-        float closestForward = 0, closestStrafe = 0, closestDifference = Float.MAX_VALUE;
-
-        for (float predictedForward = -1f; predictedForward <= 1f; predictedForward += 1f) {
-            for (float predictedStrafe = -1f; predictedStrafe <= 1f; predictedStrafe += 1f) {
-                if (predictedStrafe == 0 && predictedForward == 0) continue;
-
-                final double predictedAngle = MathHelper.wrapAngleTo180_double(Math.toDegrees(direction(yaw, predictedForward, predictedStrafe)));
-                final double difference = Math.abs(angle - predictedAngle);
-
-                if (difference < closestDifference) {
-                    closestDifference = (float) difference;
-                    closestForward = predictedForward;
-                    closestStrafe = predictedStrafe;
-                }
-            }
-        }
-
-        event.setForward(closestForward);
-        event.setStrafe(closestStrafe);
-    }
-
-    /**
-     * Gets the players' movement yaw
-     */
-    public double direction(float rotationYaw, final double moveForward, final double moveStrafing) {
-        if (moveForward < 0F) rotationYaw += 180F;
-
-        float forward = 1F;
-
-        if (moveForward < 0F) forward = -0.5F;
-        else if (moveForward > 0F) forward = 0.5F;
-
-        if (moveStrafing > 0F) rotationYaw -= 90F * forward;
-        if (moveStrafing < 0F) rotationYaw += 90F * forward;
-
-        return Math.toRadians(rotationYaw);
+        event.setForward(Math.round(forward * cosValue + strafe * sinValue));
+        event.setStrafe(Math.round(strafe * cosValue - forward * sinValue));
     }
 
     @Override 
