f850674618ee3a3930b83042bb303a42d1762e26
cranci1 <trevisanfrancesco022@gmail.com>
2024-06-03 17:14:02 +0200

    ZoneCraft + MMC autoplay

---
+++ src/main/java/net/minusmc/minusbounce/features/module/modules/misc/autoplays/other/ZoneCraftAutoPlay.kt
@@ -0,0 +1,43 @@
+package net.minusmc.minusbounce.features.module.modules.misc.autoplays.other
+
+
+import net.minusmc.minusbounce.features.module.modules.misc.autoplays.AutoPlayMode
+import net.minusmc.minusbounce.event.PacketEvent
+import net.minecraft.network.play.client.*
+import net.minecraft.network.play.server.S02PacketChat
+import net.minecraft.network.play.server.S2DPacketOpenWindow
+import net.minecraft.network.play.server.S2FPacketSetSlot
+
+/**
+ * Auto join to other match in minigames in Universocraft
+ * @author toidicakhia, cranci1
+ */
+class ZoneCraftAutoPlay: AutoPlayMode("ZoneCraft") {
+    override fun onPacket(event: PacketEvent) {
+        val packet = event.packet
+
+        if (packet !is S02PacketChat)
+            return
+
+        val text = packet.chatComponent.unformattedText
+        if (!text.contains("Volver a jugar", true))
+            return
+
+        val commandTextComponent = packet.chatComponent.siblings.firstOrNull {
+            it.unformattedText.contains("Volver a jugar", true)
+        } ?: return
+
+        val command = commandTextComponent.chatStyle.chatClickEvent.value
+        queueAutoPlay {
+            mc.thePlayer.sendChatMessage(command)
+        }
+    }
+
+    override fun onEnable() {
+        queued = false
+    }
+
+    override fun onWorld() {
+        queued = false
+    }
+} 
