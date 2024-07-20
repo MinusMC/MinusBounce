f1c31b50f71adbf384c48eb4d2c4487d2be23cda
toidicakhia <toidicakhia1234@gmail.com>
2024-07-20 13:55:42 +0700

    oops

--- src/main/java/net/minusmc/minusbounce/features/module/modules/movement/noslows/food/IntaveInventoryFoodNoSlow.kt
+++ src/main/java/net/minusmc/minusbounce/features/module/modules/movement/noslows/food/IntaveInventoryFoodNoSlow.kt
@@ -12,7 +12,7 @@ import net.minusmc.minusbounce.features.module.modules.movement.noslows.NoSlowMo
 import net.minusmc.minusbounce.utils.player.MovementUtils
 import net.minusmc.minusbounce.utils.PacketUtils
 
-class IntaveInventorySwordNoSlow : NoSlowMode("IntaveInventory") {
+class IntaveInventoryFoodNoSlow : NoSlowMode("IntaveInventory") {
     override fun onPreMotion(event: PreMotionEvent) {
         if (MovementUtils.isMoving) {
             PacketUtils.sendPacketNoEvent(C16PacketClientStatus(C16PacketClientStatus.EnumState.OPEN_INVENTORY_ACHIEVEMENT))
